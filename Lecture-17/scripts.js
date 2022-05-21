2adffe287dd89f699dcd22b00e596a8e4f08c262
SonyaSitnova <101972967+SonyaSitnova@users.noreply.github.com>
2022-05-21 11:11:12 +0300

    addEditFunc

--- Lecture-17/scripts.js
+++ Lecture-17/scripts.js
@@ -1,79 +1,83 @@
-//  ------- Edit Button ------- 
+var menuButton = document.querySelector('.EditPageButton');
+var addPersonButton = document.getElementById('AddOnePerson');
+
+menuButton.addEventListener('click', onMenuButtonPress);
+addPersonButton.addEventListener('click', onAddPersonPress);
+
+function onMenuButtonPress(){
+  var mainMenuWrapper = document.querySelector('.EditPeopleWrapper');
+
+  mainMenuWrapper.classList.toggle('EditPeopleWrapper--expanded');
+  menuButton.classList.toggle('EditPageButton--active');
+}
+
+function onAddPersonPress(){
+  var person = getPersonData();
+  addPersonCard(person);
+  clearForm();
+
+  var personCard = document.querySelector('.PersonCard');  
+  personCard.addEventListener('click', onEditPersonPress);
  
-var wrapper = document.querySelector('.EditPeopleWrapper'),
-    btnEdit = document.querySelector('.EditPageButton');
-
-function onClickEditToggle(){
-    wrapper.classList.toggle('expanded');
-    btnEdit.classList.toggle('active') 
-
-    if(btnEdit.innerText === "Stop"){
-        btnEdit.textContent = "Edit";
-    } else {
-        btnEdit.textContent = "Stop";
-    }
-}     
-
-//  ----- Input data --- 
-
-var btnAddPerson = document.getElementById('AddOnePerson');
-btnAddPerson.addEventListener('click', AddPerson);
-
-var peopleContainer = document.querySelector('.PeopleCardsContainer'),
-    peopleWrapper = document.querySelector('.PeopleCardWrapper');
-
-function AddPerson(){
-    var person = {
-            namePerson: document.getElementById('name').value ,
-            jobTitle: document.getElementById('jobTitle').value,
-            mail: document.getElementById('mail').value,
-            phone: document.getElementById('phone').value
-        };
-
-    peopleContainer.remove();
-
-// ----- Person Card Visible ------
-
-    var personCard = document.createElement('div');
-    personCard.classList.add('PersonCard');
-    peopleWrapper.appendChild(personCard);
-
-    personCard.innerHTML = 
-        '<div class="PersonImage"></div>' +
-        '<div class="PersonName">' + person.namePerson + '</div>' + 
-        '<div class="personJobTitle">' + person.jobTitle + '</div>' + 
-        '<div class="eMail">' + person.mail + '</div>'  + 
-        '<div class="PersonPhoneNumber">' + person.phone + '</div>'    
 }
 
-// ------ Clear input fields -----
+function getPersonData(){
+  var allInputElements = document.querySelectorAll('.EditPeopleInput');
+  var person = {};
 
-btnAddPerson.addEventListener('click', clearInputFields);
+  for(var i = 0; i < allInputElements.length; i++){
+    var key = allInputElements[i].getAttribute('name');
+    person[key] = allInputElements[i].value;
+  }
 
-function clearInputFields(){
-   document.getElementById('name').value = '';
-   document.getElementById('jobTitle').value = '';
-   document.getElementById('mail').value = '';
-   document.getElementById('phone').value = '';
-} 
+  return person;
+}
 
-// ----- Person Card edit ----
+function addPersonCard(person){
+  var cardsWrapper = document.querySelector('.PeopleCardWrapper');
+  var card = document.createElement('div');
+  card.classList.add('PersonCard');
 
-personCard.addEventListener('click', editPersonCard);
+  card.innerHTML = 
+    '<div class="PersonImage "></div>' + 
+    '<div class="PersonName edit" name="name"">' + person.name + '</div>' + 
+    '<div class="personJobTitle edit" name="job" >' + person.job + '</div>' + 
+    '<div class="eMail edit"  name="email">' + person.email + '</div>' + 
+    '<div class="PersonPhoneNumber edit" name="phone">' + person.phone + '</div>';
 
-function editPersonCard() {
+  cardsWrapper.appendChild(card);
+  cardsWrapper.classList.remove('empty');
+}
 
+function clearForm(){
+  var allInputElements = document.querySelectorAll('.EditPeopleInput');
+
+  for(var i = 0; i < allInputElements.length; i++){
+    allInputElements[i].value = '';
+  }
 }
 
 
-        
+function onEditPersonPress(){
+    var personEdit = getPersonFromCard();    
+    onPersonCardPress(personEdit);
+}
 
+function getPersonFromCard(){  
+    var cardDataElements = document.querySelectorAll('.edit');    
+    var personEdit = {};
 
+    for(var i =0; i < cardDataElements.length; i++){
 
+        var key = cardDataElements[i].getAttribute('name');       
+        personEdit[key] = cardDataElements[i].innerHTML;
+    }   
+   
+    return personEdit;
+}
 
+function onPersonCardPress(personEdit){
 
-
-
-
-
-
+   s
+    
+}
